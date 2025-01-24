34bd9b4cd537334c02e0c238d592145427e58d05
CodeByAhmad <devdroid47@gmail.com>
2025-01-24 20:09:56 +0500

    Test

--- src/Screens/index.js
+++ src/Screens/index.js
@@ -1,3 +1,4 @@
-export { default as Home } from './Home/Home'
-export { default as Profile } from './Profile/Profile'
-export { default as Explore } from './Explore/Explore'
+export {default as Home} from './Home/Home';
+export {default as Profile} from './Profile/Profile';
+export {default as Explore} from './Explore/Explore';
+export {default as Search} from './Search/Search';
